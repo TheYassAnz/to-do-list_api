ab2c5a582c3153394d74b95b729ed0fd3c1b65f7
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-04-25 15:42:55 +0200

    feat: crypt password with bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,31 +1,47 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 exports.signup = (req, res) => {
     delete req.body._id;
-    const user = new User({
-        ...req.body
-    });
-    user.save()
-        .then(() => res.status(201).json({ message: 'User created!' }))
-        .catch(error => res.status(400).json({ error }));
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                ...req.body,
+                password: hash,
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'User created!' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+
 };
 
 exports.login = (req, res) => {
     delete req.body._id;
-    User.findOne({ email: req.body.email, password: req.body.password })
+    User.findOne({ email: req.body.email })
         .then((user) => {
             if (!user) {
-                return res.status(401).json({ error: 'User not found!' });
+                return res.status(401).json({ error: 'Incorrect Input!' });
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            return res.status(401).json({ error: 'Incorrect Input!' });
+                        } else {
+                            res.status(200).json({
+                                userId: user._id,
+                                token: jwt.sign(
+                                    { userId: user._id },
+                                    'RANDOM_TOKEN_SECRET',
+                                    { expiresIn: '24h' }
+                                )
+                            });
+                        }
+                    })
+                    .catch(err => res.status(500).json({ err }));
             }
-            res.status(200).json({
-                userId: user._id,
-                token: jwt.sign(
-                    { userId: user._id },
-                    'RANDOM_TOKEN_SECRET',
-                    { expiresIn: '24h' }
-                )
-            });
         })
         .catch((err) => res.status(400).json({ err }));
 }
